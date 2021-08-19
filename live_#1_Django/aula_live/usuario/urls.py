05521b8c120276dd17ec5aa80a013978c6534788
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-08-19 12:27:06 -0300

    Finalizado com sucesso

--- live_#1_Django/aula_live/usuario/urls.py
+++ live_#1_Django/aula_live/usuario/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('salvar', views.salvar, name='salvar')
 ]
--- live_#1_Django/aula_live/usuario/views.py
+++ live_#1_Django/aula_live/usuario/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import  HttpResponse
+from .models import Usuario
 import json
 
 # Create your views here.
@@ -8,9 +9,26 @@ def cadastro(request):
     return HttpResponse(json.dumps({"name": "Lucas"}))
 
 def login(request):
-    nome = request.GET.get("nome")
-    sobrenome = request.GET.get("sobrenome")
-    return render(request, 'login/login.html', {'nome_usuario': nome, 'sobrenome': sobrenome})
+    status = request.GET.get("status")
+    return render(request, 'login/login.html', {'status': status})
 
 def home(request):
     return HttpResponse("Você está na página home")
+
+def salvar(request):
+    try:
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        idade = request.POST.get("idade")
+
+        usuario = Usuario (
+            nome = nome,
+            sobrenome = sobrenome,
+            idade = idade
+        )
+    
+        return redirect("/usuario/login/?status=1")
+    except:
+          return redirect("/usuario/login/?status=2")
+
+    #return HttpResponse(f"{nome} {sobrenome} {idade}")
